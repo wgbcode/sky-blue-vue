29a3690db286a530b81cfc501a35c1573469858e
wuguangbin <2567199685@qq.com>
2022-04-07 13:55:40 +0800

    consider and set 404 router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
-
-
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter)
 
@@ -24,6 +23,9 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics
+  }, {
+    path: "*",
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>你输入的网址不存在，请重新输入!</div>
+    <div><router-link to="/">返回首页</router-link></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped>
+</style>
