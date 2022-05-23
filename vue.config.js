417e3f220efe18a76ecc5eeed6465401bd8f5ae5
wuguangbin <2567199685@qq.com>
2022-05-23 15:33:49 +0800

    改寫名字格式

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sky-blue-vue/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')  // __dirname:当前目录
